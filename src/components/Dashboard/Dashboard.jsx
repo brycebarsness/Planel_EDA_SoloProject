d6a2b4bf9f0fc18a5184bac57c35ea8614a77449
Bryce Barsness <bryce5@gmail.com>
2021-02-26 10:57:31 -0600

    started table

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,5 +1,29 @@
-import React, { useState } from "react";
-import { useSelector } from "react-redux";
+import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  TextField,
+  Menu,
+  MenuItem,
+  Button,
+  makeStyles,
+  Checkbox,
+  Grid,
+  FormControl,
+  FormGroup,
+  FormLabel,
+  FormControlLabel,
+  RadioGroup,
+  Radio,
+  ButtonGroup,
+} from "@material-ui/core";
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& > *": {
+      margin: theme.spacing(2),
+      width: "50vw",
+    },
+  },
+}));
 
 // Basic functional component structure for React with default state
 // value setup. When making a new component be sure to replace the
@@ -9,10 +33,38 @@ function Dashboard(props) {
   // a default value of 'Functional Component'
   const store = useSelector((store) => store);
   const [heading, setHeading] = useState("Functional Component");
+  const classes = useStyles();
 
+  const displayAllJob = useSelector((state) => state.setAllJobsReducer);
   return (
     <div>
       <h2>{heading}</h2>
+      <h3 className={classes.root}> Click Job For Details</h3>
+
+      {displayAllJob.map((job) => {
+        return (
+          <table key={job.id}>
+            <tbody>
+              <tr>
+                <td>{job.id}</td>
+                <td>{job.user_id}</td>
+                <td>{job.contractor}</td>
+                <td>{job.street_address}</td>
+                <td>{job.city}</td>
+                <td>{job.state}</td>
+                <td>{job.zip}</td>
+                <td>{job.start_date}</td>
+                <td>{job.outside_corners}</td>
+                <td>{job.inside_corners}</td>
+                <td>{job.status}</td>
+                <td>{job.complete}</td>
+                <td>{job.comments}</td>
+                <td>{job.finish_date}</td>
+              </tr>
+            </tbody>
+          </table>
+        );
+      })}
     </div>
   );
 }
